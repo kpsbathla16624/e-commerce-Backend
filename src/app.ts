5d2362b8f1028020ba555300b878dd3203aed1c1
kamal preet <kamalpreet6198@gmail.com>
2025-03-29 02:21:44 +0530

    Add CORS support and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes';
+import cors from 'cors';
+
 
 
 
@@ -10,6 +12,7 @@ dotenv.config();
 async function start() {
     try {
         const app = express();
+        app.use(cors());
         
       
         await mongoose.connect(process.env.MONGO_URL ?? '');
@@ -18,9 +21,11 @@ async function start() {
 
         
         app.use('/api', router); 
+        app.get('/', (req :any, res:any) => {
+            return res.send('Hello World!')});
 
-        app.listen(4000, () => {
-            console.log('Server running on port 4000');
+        app.listen(3000,' 192.168.66.244', () => {
+            console.log('Server running on port 3000');
         });
        
       
